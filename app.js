4e6f20136dcf6df2f6fba4914ff7872110d47889
sps-398 <shashankpratapsingh5004@gmail.com>
2023-03-21 22:12:11 +0530

    task4 - requests/response

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log("Hi Shashank");
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My App</title></head>');
+    res.write('<body>');
+
+  if (url === '/')
+    response(res, "Welcome to my NodeJs project");
+  if (url === '/home')
+    response(res, "Welcome home");
+  if(url === '/about')
+    response(res, "Welcome to About Us page");
+  if(url === '/node')
+    response(res, "Welcome to my NodeJs project");
+
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
+
+function response(res, str) {
+    res.write(str);
+}
 
 server.listen(4000);
