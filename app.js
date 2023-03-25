b038d837d763f59d953bedfcd051b832ac8e7ffe
sps-398 <shashankpratapsingh5004@gmail.com>
2023-03-25 23:16:34 +0530

    task8-chatAppDemo

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+  var msg="";
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title><head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
+    return fs.readFile('message.txt', (err, value) => {
+        if (err) return;
+        var msg=value.toString();
+        res.write(`<body><p>${msg}</p><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>`);
+        res.write('</html>');
+        return res.end();
+   });
   }
   if (url === '/message' && method === 'POST') {
     const body = [];
@@ -17,14 +22,15 @@ const server = http.createServer((req, res) => {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on('end', () => {
+    return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err=> {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
