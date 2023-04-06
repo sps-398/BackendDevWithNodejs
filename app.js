2c00e0b29b4319fba78c68468df379fe911dac49
sps-398 <shashankpratapsingh5004@gmail.com>
2023-04-06 13:46:10 +0530

    NodeJs:Ass2-task2-Intro to Middlewares

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("Inside first middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="post"><input type="text" name="title" placeholder="Enter title"/><input type="number" name="size" placeholder="Enter size"/><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("Inside second middleware");
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Welcome to Express</h1>');
 });
 
